173c422541d66f1cab83d4cab3265b54d9c28c1c
XiaolonglongQaQ <790176569@qq.com>
2023-05-29 21:03:11 +0800

    asset

--- Source/RolePlayGame/Private/Gameplay/GCharacter.cpp
+++ Source/RolePlayGame/Private/Gameplay/GCharacter.cpp
@@ -6,12 +6,25 @@ AGCharacter::AGCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->SetupAttachment(SpringArmComp);
 }
 void AGCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
+
+void AGCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(),Value);
+}
+
+
 void AGCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
@@ -21,5 +34,7 @@ void AGCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward",this,&AGCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("Turn",this,&APawn::AddControllerYawInput);
 }
 
--- Source/RolePlayGame/Public/Gameplay/GCharacter.h
+++ Source/RolePlayGame/Public/Gameplay/GCharacter.h
@@ -2,8 +2,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "GCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
 
 UCLASS()
 class ROLEPLAYGAME_API AGCharacter : public ACharacter
@@ -12,13 +17,23 @@ class ROLEPLAYGAME_API AGCharacter : public ACharacter
 
 public:
 	AGCharacter();
-
-protected:
-	virtual void BeginPlay() override;
-
-public:	
+	
 	virtual void Tick(float DeltaTime) override;
 	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+protected:
+	virtual void BeginPlay() override;
+
+	void MoveForward(float Value);
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
+
+
+	
 
 };
